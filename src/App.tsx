75a07a2d6dd27e393f5286c744aae058749858f0
Janina Kim Marks <jmarks@Janinas-MBP.fritz.box>
2022-01-18 20:27:34 +0100

    Add background images

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import './App.css';
 import StoryHeader from "./components/story/StoryHeader";
 import StoryLine from "./components/story/StoryLine";
+import BgClouds from './assets/clouds.png';
+import MgMountains from './assets/mountains.png';
+import FgHill from './assets/hill-anna.png';
 
 function App() {
     return (
         <div className="App">
+            <img className="bg-clouds" src={BgClouds} alt={"clouds"}/>
+            <img className="mg-mountains" src={MgMountains} alt={"mountains"}/>
+            <img className="fg-hill" src={FgHill} alt={"hill"}/>
+            <div className="gradient-top"/>
+            <div className="gradient-middle"/>
             <StoryHeader
                 title={"Seid dabei auf unserem ersten großen Abenteuer"}
                 subtitle={"30 Tage Portugal & Spanien"}
