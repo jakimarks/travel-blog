7a6a65eec6dde06a0029715d1274b598e426982d
Janina Kim Marks <jakimarks@gmail.com>
2022-01-21 06:59:16 +0100

    Add logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import React, {useEffect} from 'react';
 import './App.css';
 import WebFont from 'webfontloader';
 import Story from "./components/story/Story";
+import Logo from "./components/branding/Logo";
 
 function App() {
     useEffect(() => {
         WebFont.load({
             google: {
-                families: ['Abhaya Libre', 'Amiko']
+                families: ['Abhaya Libre', 'Amiko', 'Amatic SC']
             }
         });
     }, []);
 
     return (
         <div className="app">
+            <Logo/>
             <Story/>
         </div>
     );
---
+++ src/components/branding/Logo.tsx
@@ -0,0 +1,12 @@
+import './Logo.css'
+
+function Logo() {
+    return (
+        <div className="logo">
+            <span className="logo__arrows">→←→</span>
+            <span className="logo__text">Abenteuer</span>
+        </div>
+    )
+}
+
+export default Logo
