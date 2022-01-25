a2a2edc9036999d5a8d008446d6ce74c35f878f1
Janina Kim Marks <jakimarks@gmail.com>
2022-01-25 20:49:59 +0100

    Create basic footer

--- src/App.tsx
+++ src/App.tsx
@@ -39,6 +39,20 @@ function App() {
                 <NavBar expand={responsive.expandNavBar}/>
             </div>
             <Story/>
+            <div className="footer">
+                <div className="footer__left">
+                    <Logo/>
+                    <span>"Einfach mal machen, könnte ja gut werden."</span>
+                </div>
+                <div className="footer__right">
+                    <span className="footer__right__title">Mehr auf unserem Blog</span>
+                    <div className="footer__right__links">
+                        <a className="footer__right__link" href="#">Kontakt</a>
+                        <a className="footer__right__link" href="#">Impressum</a>
+                        <a className="footer__right__link" href="#">Datenschutzerklärung</a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
