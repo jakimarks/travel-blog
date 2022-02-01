112768a1919ad00a2423c4c6ac2a63673c6e489c
Janina Kim Marks <jakimarks@gmail.com>
2022-02-01 22:05:15 +0100

    Fix scroll link for story header

--- src/components/story/Story.tsx
+++ src/components/story/Story.tsx
@@ -9,16 +9,18 @@ import StoryLine from "./StoryLine";
 function Story() {
     return (
         <div className="story">
-            <div className="story__background">
-                <img className="story__bg-clouds" src={BgClouds} alt={"clouds"}/>
-                <img className="story__mg-mountains" src={MgMountains} alt={"mountains"}/>
-                <img className="story__fg-hill" src={FgHill} alt={"hill"}/>
-                <div className="story__gradient-top"/>
+            <div className="story-spacer">
+                <div className="story__background">
+                    <img className="story__bg-clouds" src={BgClouds} alt={"clouds"}/>
+                    <img className="story__mg-mountains" src={MgMountains} alt={"mountains"}/>
+                    <img className="story__fg-hill" src={FgHill} alt={"hill"}/>
+                    <div className="story__gradient-top"/>
+                </div>
+                <StoryHeader
+                    title={"Seid dabei auf unserem ersten großen Abenteuer"}
+                    subtitle={"30 Tage Portugal & Spanien"}
+                />
             </div>
-            <StoryHeader
-                title={"Seid dabei auf unserem ersten großen Abenteuer"}
-                subtitle={"30 Tage Portugal & Spanien"}
-            />
             <StoryLine/>
         </div>
     )
